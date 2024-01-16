087b7a3428268b66f7f05ce820446f8e7c83a8c4
shafhan farizi_TI01 <127157200+shafhan-farizi@users.noreply.github.com>
2024-01-16 13:39:56 +0700

    Salah Folder

--- projek/php/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $result = '=' . $username . '/' . $password;
-    
-    file_put_contents('../data/user.txt', file_get_contents('../data/user.txt') . $result);
-    header('Location: ../login.html');
-?>
